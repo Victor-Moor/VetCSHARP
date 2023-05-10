11a83f7930de8dd91e6e6c7159429928d25ef965
Vol-Power <victor.moor@live.com>
2023-05-09 21:37:06 -0300

    Nome da classe

--- Curso/Curso/Quartos.cs
+++ Curso/Curso/Quartos.cs
@@ -8,5 +8,15 @@ namespace Curso
 {
     internal class Quartos
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Quartos(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
     }
 }
