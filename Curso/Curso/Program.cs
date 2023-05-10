17b5af524819bf14edc5c9e65b182bc0c17547ad
Vol-Power <victor.moor@live.com>
2023-05-09 21:34:01 -0300

    fazendo a main

--- Curso/Curso/Program.cs
+++ Curso/Curso/Program.cs
@@ -10,6 +10,32 @@ namespace Curso
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Quantos quartos vão ser alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Quartos[] vet = new Quartos[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Reserva #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Quartos(nome, email, quarto);
+            }
+
+            Console.WriteLine("Quartos alugados");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(vet[i].Quarto + ": " + vet[i].Nome + ", " + vet[i].Email);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
